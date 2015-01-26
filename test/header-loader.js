ef89f01165064a5659b46b6f4dc66a56eae40597
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-01-26 15:35:27 +0100

    Js loader

--- test/header-loader.js
+++ test/header-loader.js
@@ -14,31 +14,42 @@ WebFontConfig = {
 
 // Load html & css
 (function () {
-  var links = {
-    html: 'https://cdn.rawgit.com/simbiotica/gfw_assets/feature/loading-by-js/test/header_test.html',
-    css: 'https://rawgit.com/simbiotica/gfw_assets/feature/loading-by-js/test/header-gfw.css'
-  }
+    var scriptPram = document.getElementById('loader-gfw');
+    var current = scriptPram.getAttribute('data-current');
 
-  var header = document.getElementById('headerGfw'),
-      head = document.head || document.getElementsByTagName('head')[0], 
-      xhrHtml = new XMLHttpRequest(),
-      xhrCss = new XMLHttpRequest();
-  xhrHtml.onreadystatechange = function (e) { 
-    if (xhrHtml.readyState == 4 && xhrHtml.status == 200) {
-      header.innerHTML = xhrHtml.responseText;
+    var links = {
+        html: 'https://cdn.rawgit.com/simbiotica/gfw_assets/feature/loading-by-js/test/header_test.html',
+        css: 'https://rawgit.com/simbiotica/gfw_assets/feature/loading-by-js/test/header-gfw.css'
     }
-  }
-  xhrCss.onreadystatechange = function (e) { 
-    if (xhrCss.readyState == 4 && xhrCss.status == 200) {
-      var style = document.createElement('style');
-          style.type = 'text/css';
-          style.appendChild(document.createTextNode(xhrCss.responseText));
-      // Append to header
-      head.appendChild(style);
-      xhrHtml.open("GET", links.html, true);
-      xhrHtml.send();
+
+    var header = document.getElementById('headerGfw'),
+        head = document.head || document.getElementsByTagName('head')[0], 
+        xhrHtml = new XMLHttpRequest(),
+        xhrCss = new XMLHttpRequest();
+        href = location.href;
+
+    xhrCss.open("GET", links.css, true);
+    xhrCss.send();
+
+    xhrCss.onreadystatechange = function (e) { 
+        if (xhrCss.readyState == 4 && xhrCss.status == 200) {
+            var style = document.createElement('style');
+                style.type = 'text/css';
+                style.appendChild(document.createTextNode(xhrCss.responseText));
+            // Append to header
+            head.appendChild(style);
+            xhrHtml.open("GET", links.html, true);
+            xhrHtml.send();
+        }
     }
-  }
-  xhrCss.open("GET", links.css, true);
-  xhrCss.send();
+
+    xhrHtml.onreadystatechange = function (e) { 
+        if (xhrHtml.readyState == 4 && xhrHtml.status == 200) {
+            header.innerHTML = xhrHtml.responseText;
+            headerUl = document.getElementById('headerUl');
+            console.log(headerUl);
+            headerUl.find('.shape-fire').addClass('current');
+        }
+    }
+
 }());
