a7441b11596556b676f88a8a462a84a0e7be2bc0
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-04-01 16:19:52 +0200

    Footer and css updated

--- src/header-loader.js
+++ src/header-loader.js
@@ -35,9 +35,9 @@ function googleTranslateElementInit() {
     var current = scriptPram.getAttribute('data-current');
 
     var links = {
-        htmlHeader: 'https://cdn.rawgit.com/simbiotica/gfw_assets/master/src/header.html',
-        htmlFooter: 'https://cdn.rawgit.com/simbiotica/gfw_assets/master/src/footer.html',
-        css: 'https://cdn.rawgit.com/simbiotica/gfw_assets/master/src/gfw-styles.css',
+        htmlHeader: 'https://rawgit.com/simbiotica/gfw_assets/master/src/header.html',
+        htmlFooter: 'https://rawgit.com/simbiotica/gfw_assets/master/src/footer.html',
+        css: 'https://rawgit.com/simbiotica/gfw_assets/master/src/gfw-styles.css',
         translate: 'http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
     }
 
