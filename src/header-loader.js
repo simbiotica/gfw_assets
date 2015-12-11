04c51253b8986094de5246454cbc57a72e74430a
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-12-11 11:37:41 +0100

    Header loader updated

--- src/header-loader.js
+++ src/header-loader.js
@@ -32,9 +32,9 @@ function googleTranslateElementInit() {
     var current = (scriptPram) ? scriptPram.getAttribute('data-current') : '.home_data';
 
     var links = {
-        htmlHeader: 'src/header.html',
-        htmlFooter: 'src/footer.html',
-        css: 'src/gfw-styles.css',
+        htmlHeader: 'https://cdn.rawgit.com/simbiotica/gfw_assets/57981e4612067974e8f94e606e2adb6ab0a9a99a/src/header.html',
+        htmlFooter: 'https://cdn.rawgit.com/simbiotica/gfw_assets/57981e4612067974e8f94e606e2adb6ab0a9a99a/src/footer.html',
+        css: 'https://cdn.rawgit.com/simbiotica/gfw_assets/57981e4612067974e8f94e606e2adb6ab0a9a99a/src/gfw-styles.css',
         translate: 'http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
     }
 
