a47514afccdedf183eeb99010b5b80ca3f6a9557
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-12-14 16:01:03 +0100

    Console remove

--- js/app/loader.js
+++ js/app/loader.js
@@ -1,9 +1,9 @@
 var loader = {
 
   urls: {
-    header: 'https://raw.githubusercontent.com/simbiotica/gfw_assets/develop/src/header.html',
-    footer: 'https://raw.githubusercontent.com/simbiotica/gfw_assets/develop/src/footer.html',
-    css: 'https://raw.githubusercontent.com/simbiotica/gfw_assets/develop/css/build/minified/global.css',
+    header: 'https://cdn.rawgit.com/simbiotica/gfw_assets/29205eb50fe9b4a578f230614defa4724c4fff55/src/header.html',
+    footer: 'https://cdn.rawgit.com/simbiotica/gfw_assets/b53f31b50a680476924a61f883376e14407b4c5c/src/footer.html',
+    css: 'https://cdn.rawgit.com/simbiotica/gfw_assets/de63d92ae18e9d449474bd9ba614c259c14c21b5/css/build/global.css',
     slick: 'http://cdn.jsdelivr.net/jquery.slick/1.3.15/slick.min.js',
     // translate: 'http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
   },
