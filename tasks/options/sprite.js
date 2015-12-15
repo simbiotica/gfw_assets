62ef80d9de4f8d48c9313abb606d12b6e771b973
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-12-15 12:35:06 +0100

    Production mode

--- tasks/options/sprite.js
+++ tasks/options/sprite.js
@@ -3,7 +3,8 @@ module.exports =  {
   all: {
     src: 'images/logos/*.png',
     dest: 'images/sprites/logos.png',
-    destCss: 'css/modules/logos.scss'
+    destCss: 'css/modules/logos.scss',
+    imgPath: 'https://cdn.rawgit.com/simbiotica/gfw_assets/39365520305b62793c15efa9fefc712206e21493/images/sprites/logos.png'
   }
 
 };
