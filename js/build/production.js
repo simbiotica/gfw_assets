61f8ac36db2b2d3a67ff05c081c4d530b29cee89
Adam Mulligan <adammulligan@gmail.com>
2016-01-14 09:58:19 +0000

    Non dev loader

--- js/build/production.js
+++ js/build/production.js
@@ -643,7 +643,7 @@ var loaderGFW = {
     this.promises = [];
     // If you want to develop you should change this.urls => this.urls_dev
     // It may be better if we set this var by branches
-    $.each(this.urls_dev, function(k,v){
+    $.each(this.urls, function(k,v){
       var deferred = new $.Deferred();
       $.ajax({
         url: v,
