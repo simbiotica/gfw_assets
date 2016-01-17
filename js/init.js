82a51b6369f327f60ab89046080d08f392a3eb33
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-17 23:20:28 +0100

    font loaded by css (I do not feel very comfortable with this solution, it blocks the rest of the load)

--- js/init.js
+++ js/init.js
@@ -1,16 +1,3 @@
-// Load fonts
-WebFontConfig = {
-  google: { families: [ 'Fira+Sans:300,400,500:latin' ] }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})(); 
-
 // Google translate
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
