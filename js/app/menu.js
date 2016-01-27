eee5c1db6d43e0760459b9c697b15a4ee2766f2d
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-27 17:31:09 +0100

    Now we have the possibility of change the menu depending on the app

---
+++ js/app/menu.js
@@ -0,0 +1,32 @@
+var menuGFW = {
+  init: function(params) {
+    this.cacheVars();
+    // Choose the menu you want to render
+    this.switchMenu();
+    // Print it
+    this.printMenu();
+  },
+
+  cacheVars: function() {
+    this.$mobileMenu = $('#submenuMenuMobile');
+  },
+
+  switchMenu: function() {
+    switch(location.hostname) {
+      case 'localhost':
+        this.menu = menujsonGFW['default'];
+      break;
+
+      case 'climate.globalforestwatch.org':
+        this.menu = menujsonGFW['climate'];
+      break;
+
+      default: 
+        this.menu = menujsonGFW['default'];
+    }
+  },
+
+  printMenu: function() {
+    this.$mobileMenu.html(tmplGFW('mobileMenuTPL',{ menu: this.menu }));
+  }
+}
